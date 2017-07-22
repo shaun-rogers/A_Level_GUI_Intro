eb8298cfd0900527f283b7321f29fe25a884f60f
shaunrogers <srogers@plymstockschool.co.uk>
2017-07-22 17:25:47 +0100

    Initial commit

---
+++ 1 - The Basics.py
@@ -0,0 +1,19 @@
+'''Creator: S Rogers
+Title: 1 - The Basics
+
+Description: This program is to be used with Year 12 students to introduce them
+to the basics of Tkinter GUI.
+
+They will learn about importing the module, initialising a window
+and how to use some basic widgets'''
+
+# This line imports the class "tkinter" and all of its methods
+# The "as tk" is a common approach to make calling methods faster
+import tkinter as tk
+
+# root could be called anything and simply provides an identifier for the main window
+root = tk.Tk()
+
+# This method intiates the loop that allows the GUI to actually run
+root.mainloop()
+
