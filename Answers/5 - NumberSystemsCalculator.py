8bc3da102ad90917c9c3a137f6de8135f79b7216
srogers <srogers@plymstockschool.org.uk>
2017-09-15 15:23:46 +0100

    Removed ne GUI element. Add this later
    Sorted errors in validation and error checking for each function.
    
    Testing needed

--- Answers/5 - NumberSystemsCalculator.py
+++ Answers/5 - NumberSystemsCalculator.py
@@ -5,7 +5,8 @@ Purpose: Demonstration of how to create a GUI for a simple APP.
 This app allows the user to enter a number in Decimal, Binary or Hex and convert
 to a number system of their choice
 
-To Do: 15/03/17 Error checking still not working on Bin_Dec and Bin_Hex
+To Do: Test with normal and extreme values
+To Do: Adapt the app so that there is a home screen, num converter screen and test screen.
 '''
 
 # This line imports the class "tkinter" and all of its methods
@@ -16,40 +17,34 @@ import string
 """This first section contains the functions
 that make the program convert the different number systems"""
 
-"""This procedure accepts the parameter subProgram which will tell it which conversion function
-to call. These functions will then return a value to outputUpdate and
-set resultText to the appropriate message"""
+#This procedure accepts the parameter subProgram which will tell it which conversion function
+#to call. These functions will then return a value to outputUpdate and
+#set resultText to the appropriate message"""
 def outputUpdate(subProgram):
     #Selection block that will run the appropriate function based upon
     #the button the user pushes
     #It first obtains the entered value in the input box
     number = baseNumber.get()
     if subProgram == 1:
-        #This validation method has been commented in the first elif of this block
         value = hex_bin()
-        test = value.replace(" ","")
-        if value.isdigit():
+        if value != "Must only contain Numbers and Letters":
             resultText.set("The binary for this number is: " + str(value)[2:].upper())
         else:
             resultText.set(value)
+
     elif subProgram == 2:
         #The function is run within a variable to that the returned
         #value is stored and usable
         value = hex_dec()
-        #Stripping the returned value of any spaces to allow the isalpha test
-        test = value.replace(" ","")
-        #If the returned value is is text isalpha() will evaluate to True
-        if test.isalpha():
-            #This message will run on isalpha() evaluating to true
-            #as the user had not entered a valid Hex value
-            resultText.set(value)
+        if value != "Must only contain Numbers and Letters":
+            resultText.set("The decimal for this number is: " + str(value).upper())
         else:
-            resultText.set("The decimal for this number is: " + str(value))
+            resultText.set(value)
     elif subProgram == 3:
         value = dec_hex()
         #using the is digit method to see if the returned value is a number.
         #If the value is a number the user has entered a valid decimal value
-        if value.isdigit():
+        if value != "Must only enter whole numbers e.g. 1, 10, 14":
             resultText.set("The decimal for this number is: " + str(value).upper())
         else:
             #If the user did not enter a valid decimal value
@@ -65,68 +60,42 @@ def outputUpdate(subProgram):
             resultText.set("The binary value of " + str(number) + " is " + str(value)[2:])
     elif subProgram == 5:
         value = bin_dec()
-        resultText.set("The decimal value of " + str(number) + " is " + str(value))
+        if value != "Must enter a valid binary number i.e. only containint 1 or 0":
+            resultText.set("The decimal value of " + str(number) + " is " + str(value))
+        else:
+            resultText.set(value)
     else:
         value = bin_hex()
-        resultText.set("The hexadecimal value of " + str(number) + " is " + str(value)[2:].upper())
+        if value != "Must enter a valid binary number i.e. only containint 1 or 0":
+            resultText.set("The hexadecimal value of " + str(number) + " is " + str(value)[2:].upper())
+        else:
+            resultText.set(value)
 
-#Completed
 def hex_bin():
     #This makes use of the hex_dec function to get the decimal value of the hex number
     #This means I don't have to re-write code
     number = hex_dec()
-    binValue = "Must only enter numbers and letters"
-    #Returning the value to the output function
-    return binValue
+    try:
+        binValue = bin(number)
+        #Returning the value to the output function
+        return binValue
+    except:
+        return "Must only contain numbers and letters in the Hex set\n" \
+                "0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f"
 
-#Completed'''
+
 def hex_dec():
     #Establish a dictionary to store the hex value of each position
-    hex_val = {"0":0, "1":1, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "A":10, "B":11, "C":12, "D":13, "E":14, "F":15}
-    #Get the value that has been entered into input field in the GUI stored in the
-    #baseNumber variable
     number = baseNumber.get()
-    #Input Sanitisation ensuring that we are working with upper case text
-    number = number.upper()
-    #VALIDATION - ensuring the user has not entered an invalid character into the input field
-    #i.e. no symbols such as !@#$ etc
-    invalidChars = set(string.punctuation)
-    #Selection statement that outputs an error message should the user enter an invalid value
-    if any(char in invalidChars for char in number):
-        value = "Must only contain Numbers and Letters"
-    else:
-        #If the entry is valid the rest of the algorithm runs
-        #Establishing a list to hold the entered value in reverse
-        #This is so that we can work from right to left i.e. as numbers work
-        number_split = []
-        #This list will store the decimal exponent of that position in a number
-        #e.g. 16,1 for a two digit Hex value
-        position_val = []
-        #Count controlled loop initiated to build the list of Hex items
-        #Count controlled used to allow it to expand for any length of entered value
-        for i in range(len(number)):
-            #.append method adds value to number_split
-            number_split.append(number[i])
-        #This process is repeated for the position values
-        for i in range(len(number)):
-            #The exponent operator is used as each position will
-            position_val.append(16**i)
-        #Mathematically the first appended value would be 0, it needs to be 1
-        #So the index of 0 is changed to 1
-        position_val[0] = 1
-        #As the position values work from right to left
-        position_val.reverse()
-        #Initialising a variable to store the actual decimal value of the number
-        value = 0
-        #Again, count controlled iteration to add the value of each position to the total
-        for i in range(len(number)):
-            value += hex_val[number_split[i]] * position_val[i]
-        #Concatenating the value into a string that can be presented in the output
+    try:
+        value = int(number,16)
+        return value
+    except:
+        value = "Must only contain numbers and letters in the Hex set\n" \
+                "0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f"
+        return value
 
-    #Returning the value to the calling function to be used there
-    return value
 
-'''Completed'''
 def dec_hex():
     #As before this is getting the entered value
     number = baseNumber.get()
@@ -147,7 +116,9 @@ def dec_hex():
 
 '''Completed Not Commented'''
 def dec_bin():
+    #Retrieving the value entered by the user to the GUI
     number = baseNumber.get()
+    #Selection statement testing if the value etered was a digit
     if number.isdigit():
         number = bin(int(number))
     else:
@@ -157,28 +128,27 @@ def dec_bin():
 '''In Progress'''
 def bin_hex():
     decValue = bin_dec()
-    hexVal = hex(int(decValue))
-    return hexVal
+    try:
+        hexVal = hex(int(decValue))
+        return hexVal
+    except:
+        return "Must enter a valid binary number i.e. only containint 1 or 0"
 
 
 
 def bin_dec():
     number = baseNumber.get()
-    value = int(number , 2)
-    return value
+    try:
+        value = int(number , 2)
+        return value
+    except:
+        return "Must enter a valid binary number i.e. only containint 1 or 0"
 
 #Setting the tk environment to start the GUI
 root = tk.Tk()
 '''I have set up different frames to allow for different grid layouts'''
 #Setting the title that will appear at the top of the window
 root.title("BinHexDec Calculator")
-
-#Establishing the navigation bar to control movement between windows in the application
-navBar = tk.Frame(root, width=400, height=50)
-navBar.pack()
-homeButton = tk.Button(navBar, text="Home", anchor= "w").grid(row=0,column=0,sticky = "W,E")
-numConvertButton = tk.Button(navBar, text="Number Converter").grid(row=0,column=1)
-testButton = tk.Button(navBar,text = "Take a Test").grid(row=0,column=2)
 
 
 
